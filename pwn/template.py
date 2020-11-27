51f5d985fbcc4e22b85d2f525209d376dcd081bd
hannibal-smith <65942818+hannibal-smith@users.noreply.github.com>
2020-11-27 11:27:37 +0200

    Create template.py

---
+++ pwn/template.py
@@ -0,0 +1,18 @@
+from pwn import *
+
+# tmux split two have all in one window
+context(terminal=['tmux', 'splitw', '-h'])
+
+#set information level to debug
+context.log_level = 'debug'
+
+#TODO: replace <ELF-FILENMAE> with name of elf-file
+io = context.binary = ELF('<ELF-FILENAME>')
+
+# start in gdb or not
+#io = gdb.debug(io.path)
+io = process(io.path)
+
+payload = ""
+
+io.interactive()
